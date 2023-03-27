e3b0495d8e1cca7c08e979d611aff0983995629a
Maurelian <maurelian@protonmail.ch>
2023-03-27 16:30:17 -0400

    feat(ctb): generate deploy config

--- packages/contracts-bedrock/tasks/generate-deploy-config.ts
+++ packages/contracts-bedrock/tasks/generate-deploy-config.ts
@@ -1,6 +1,4 @@
 import fs from 'fs'
-import path from 'path'
-
 import { task } from 'hardhat/config'
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 
@@ -13,7 +11,12 @@ task(
     if (fs.existsSync(`${base}.ts`)) {
       // eslint-disable-next-line @typescript-eslint/no-var-requires
       const config = require(`${base}.ts`).default
-      fs.writeFileSync(`${base}.json`, JSON.stringify(config, null, 2), 'utf8')
+      console.log('config:', config)
+      fs.writeFileSync(
+        `${base}.json`,
+        JSON.stringify(config, null, 2),
+        'utf8'
+      )
     } else {
       throw new Error('not found')
     }
